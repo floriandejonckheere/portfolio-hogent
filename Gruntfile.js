8157ab2d531425e4ccd0aebcabc15e89fd82adc3
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-11-26 15:04:58 +0100

    Base style

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,8 @@ module.exports = function(grunt) {
           },
           dist: {
               files: {
-                  'css/application.css': 'scss/application.scss'
+                  'css/application.css': 'scss/application.scss',
+                  'css/reset.css': 'scss/reset.scss'
               }
           }
       },
