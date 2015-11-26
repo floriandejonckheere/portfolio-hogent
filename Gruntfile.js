62c9715f8ab2683aff64b78a343b68ced50e1ad6
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-11-26 14:01:26 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+  grunt.initConfig({
+      sass: {
+          options: {
+              sourceMap: true
+          },
+          dist: {
+              files: {
+                  'css/application.css': 'scss/application.scss'
+              }
+          }
+      },
+      watch: {
+        css: {
+          files: '**/*.scss',
+          tasks: ['sass']
+        }
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['sass', 'watch']);
+}
