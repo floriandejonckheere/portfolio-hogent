4cc7d0a6eb82dc0665a912eecb756f0c766979ef
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-11-26 15:27:40 +0100

    Added Grunt templating

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,19 +8,39 @@ module.exports = function(grunt) {
           },
           dist: {
               files: {
-                  'css/application.css': 'scss/application.scss',
-                  'css/reset.css': 'scss/reset.scss'
+                  'dist/css/application.css': 'scss/application.scss',
+                  'dist/css/reset.css': 'scss/reset.scss'
               }
           }
       },
       watch: {
         css: {
-          files: '**/*.scss',
-          tasks: ['sass']
+          files: ['scss/**/*.scss', 'views/**/*', 'images/**/*'],
+          tasks: ['sass', 'includes', 'copy']
+        }
+      },
+      includes: {
+        files: {
+          src: ['views/*.html'],
+          dest: 'dist',
+          flatten: true,
+          cwd: '.',
+          options: {
+            silent: true
+          }
+        }
+      },
+      copy: {
+        main: {
+          files: [
+            { expand: true, src: ['images/**'], dest: 'dist/images/' },
+          ]
         }
       }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-includes');
+  grunt.registerTask('default', ['watch']);
 }
