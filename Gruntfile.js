cf9f64a256b11b2ec0c3236d13b8b28b77df3d6e
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-11-27 11:19:48 +0100

    Grunt deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,49 +2,62 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
   grunt.initConfig({
-      sass: {
-          options: {
-              sourcemap: "auto",
-              trace: true,
-              style: "expanded",
-              lineNumbers: false
-          },
-          dist: {
-              files: {
-                  'dist/css/application.css': 'scss/application.scss',
-                  'dist/css/reset.css': 'scss/reset.scss'
-              }
-          }
-      },
-      watch: {
-        css: {
-          files: ['scss/**/*.scss', 'views/**/*', 'images/**/*'],
-          tasks: ['sass', 'includes', 'copy']
+    sass: {
+        options: {
+            sourcemap: "auto",
+            trace: true,
+            style: "expanded",
+            lineNumbers: false
+        },
+        dist: {
+            files: {
+                'dist/css/application.css': 'scss/application.scss',
+                'dist/css/reset.css': 'scss/reset.scss'
+            }
         }
-      },
-      includes: {
-        files: {
-          src: ['views/*.html'],
-          dest: 'dist',
-          flatten: true,
-          cwd: '.',
-          options: {
-            silent: true
-          }
-        }
-      },
-      copy: {
-        main: {
-          files: [
-            { expand: true, src: ['images/**/*'], dest: 'dist/' },
-          ]
+    },
+    watch: {
+      css: {
+        files: ['scss/**/*.scss', 'views/**/*', 'images/**/*'],
+        tasks: ['sass', 'includes', 'copy']
+      }
+    },
+    includes: {
+      files: {
+        src: ['views/*.html'],
+        dest: 'dist',
+        flatten: true,
+        cwd: '.',
+        options: {
+          silent: true
         }
       }
+    },
+    copy: {
+      main: {
+        files: [
+          { expand: true, src: ['images/**/*'], dest: 'dist/' },
+        ]
+      }
+    },
+    'ftp-deploy': {
+      build: {
+        auth: {
+          host: 'users.hogent.be',
+          port: 21,
+          authKey: 'production'
+        },
+        src: 'dist/',
+        dest: '/www/'
+      }
+    }
   });
 
+  grunt.loadNpmTasks('grunt-ftp-deploy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-includes');
   grunt.registerTask('default', ['watch']);
+  grunt.registerTask('deploy', ['ftp-deploy:build']);
 }
