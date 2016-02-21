c9d35b6579a277d9a498de1f84e220ff0b85e71f
Florian Dejonckheere <florian@floriandejonckheere.be>
2016-02-21 13:00:56 +0100

    Grunt build system

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+  grunt.initConfig({
+    sass: {
+        options: {
+            sourcemap: "auto",
+            trace: true,
+            style: "expanded",
+            lineNumbers: false
+        },
+        dist: {
+            files: {
+                'dist/css/application.css': 'src/scss/application.scss',
+                'dist/css/reset.css': 'src/scss/reset.scss'
+            }
+        }
+    },
+    watch: {
+      css: {
+        files: ['src/scss/**/*.scss', 'src/html/**/*', 'src/files/**/*'],
+        tasks: ['sass', 'includes', 'copy']
+      }
+    },
+    includes: {
+      files: {
+        src: ['src/html/*.html'],
+        dest: 'dist'/,
+        flatten: true,
+        cwd: '.',
+        options: {
+          silent: true,
+          includePath: 'src/html/partials',
+          filenamePrefix: '_',
+          filenameSuffix: '.html',
+          includeRegex: /^(\s*)\@include\s+"(\S+)"\s*$/
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          { expand: false, src: ['src/files/**/*'], dest: 'dist/' }
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-includes');
+  grunt.registerTask('default', ['watch']);
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# README
+
+This is my minimalist cover page. It uses a Grunt build system in order to compile the SCSS stylesheets. The source HTML files are in the `src/html` directory, which uses a templating system to insert the headers and footers in each document. The source SCSS files are in the `src/scss` folder.
+
+The compiled files are available in the `dist` folder.
+
+## Building
+
+Development:
+
+```
+$ grunt watch
+```
